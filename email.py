e123c07e60b6d781c511d9224075d602618c8e7b
Anupam Govind <anupam.govind.1997@gmail.com>
2018-11-30 18:03:05 +0530

    added email.py

---
+++ email.py
@@ -0,0 +1,16 @@
+'''class for email objects which will be used for posting'''
+
+class attachment:
+	def __init__(self,name,url):
+		self.name = name
+		self.url = url
+
+class email:
+
+	#if any of the attribte is not present , pass None.
+	def __init__(self,name, email_addr, subject, body, attachment_list):
+		self.name = name;
+		self.email_addr = email_addr
+		self.subject = subject
+		self.body = body
+		self.attachment_list = attachment_list
