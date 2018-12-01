a689826a10890da61f0592812fdc828ed48592bd
Saurav Chirania <saurav.chir@gmail.com>
2018-12-02 01:38:32 +0530

    upload files to db, add attachtype.py

--- read_emails.py
+++ read_emails.py
@@ -2,12 +2,14 @@ import imaplib
 import email
 import email_class
 import os
+import db
 
-ORGANISATION = "@gmail.com"
-EMAIL = "xxxxxxxx" + ORGANISATION
-PASSWORD = "xxxxxxxx"
+ORGANISATION = input()
+EMAIL = input() + ORGANISATION
+PASSWORD = input()
 IMAP_SERVER = "imap.gmail.com"
-ATTACHMENT_DIR = "/home/anupam/naarad_project/Naarad-iit-ism/attachment"
+ATTACHMENT_DIR = "attachments"
+DROPBOX_ACCESS_TOKEN = input()
 
 def get_body(msg):
     if msg.is_multipart():
@@ -16,6 +18,7 @@ def get_body(msg):
         return msg.get_payload(None,True)
 
 def get_attachments(msg, mailobj):
+    uploader= db.Uploader()
     for part in msg.walk():
         if part.get_content_maintype()=='multipart':
             continue
@@ -27,18 +30,20 @@ def get_attachments(msg, mailobj):
             filePath = os.path.join(ATTACHMENT_DIR, fileName)
             with open(filePath,'wb') as f:
                 f.write(part.get_payload(decode=True))
-            attachment_obj = email_class.attachment(fileName, filePath)
+            if (len(mailobj.attachment_list)==0):
+                uploader.connect_to_server(DROPBOX_ACCESS_TOKEN)
+            link = uploader.upload_file(fileName, filePath)
+            attachment_obj = email_class.attachment(fileName, link)
             mailobj.attachment_list.append(attachment_obj)
 
-
 def readmail():
-    mail_list = []
     connection = imaplib.IMAP4_SSL(IMAP_SERVER)
     connection.login(EMAIL, PASSWORD)
     connection.select('inbox')
-    return_code , data = connection.search(None, '(FROM "Anupam" UNSEEN)')
+    return_code , data = connection.search(None, '(FROM "Saurav" UNSEEN)')
     mail_ids_string = data[0]
     mail_ids = mail_ids_string.split()
+    mail_obj_list = []
     for mail_id in mail_ids:
         typ, response = connection.fetch(mail_id, '(RFC822)' )
         for response_part in response:
@@ -53,5 +58,5 @@ def readmail():
                 print(mailobj.subject)
                 print(mailobj.body)
                 print("---------------------")
-                mail_list.append(mailobj)
-    return mail_list
+                mail_obj_list.append(mailobj)
+    return mail_obj_list
